6c25a4a05c5b0c345096a2ff118285b6cb40aed8
junseok93 <86007685+junseok93@users.noreply.github.com>
2021-10-25 15:45:36 +0900

    START

---
+++ src/main/java/com/project/contap/ContapApplication.java
@@ -0,0 +1,13 @@
+package com.project.contap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContapApplication.class, args);
+    }
+
+}
