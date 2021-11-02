654d9ecf564565c397dd8bc1eeb82650c7f738d6
junseok93 <86007685+junseok93@users.noreply.github.com>
2021-11-02 17:15:01 +0900

    remove chat

--- src/main/java/com/project/contap/chat/repository/ChatMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.contap.chat.repository;
-
-import com.project.contap.chat.model.ChatMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMemberRepository extends JpaRepository<ChatMember, Long> {
-    ChatMember findByMemberIdAndRoomId(Long memberId, String roomId);
-}
--- src/main/java/com/project/contap/chat/repository/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.contap.chat.repository;
-
-import com.project.contap.chat.model.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-
-}
