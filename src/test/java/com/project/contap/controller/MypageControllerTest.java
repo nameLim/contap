c77dec83fe51204b9be55fdc00d9644afb8f5c3d
lim49435150@gmail.com <zeze49435150>
2021-11-17 10:04:29 +0900

    테스트케이스 작성 중

--- src/test/java/com/project/contap/controller/MypageControllerTest.java
+++ src/test/java/com/project/contap/controller/MypageControllerTest.java
@@ -1,26 +1,72 @@
 package com.project.contap.controller;
 
-
 import com.project.contap.model.user.User;
 import com.project.contap.security.WebSecurityConfig;
 import com.project.contap.service.MypageService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = MypageController.class,
-excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfig.class))
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfig.class))
 class MypageControllerTest {
     @Autowired
-    MockMvc mvc;
+    private MockMvc mvc;
 
-    @MockBean
+    @Autowired
     private MypageService mypageService;
 
     User testUser;
 
+    @BeforeEach
+    public void setup() {
+//        testUser = new User("testUse@gmail.com","1234qwer","testUser");
+    }
 
+    @Nested
+    @DisplayName("")
+    class Post {
+        @Nested
+        @DisplayName("")
+        class PostSuccess {
+        }
+
+        @Nested
+        @DisplayName("")
+        class PostFail {
+        }
+    }
+
+    @Nested
+    @DisplayName("")
+    class Get {
+        @Nested
+        @DisplayName("")
+        class GetSuccess {
+        }
+
+        @Nested
+        @DisplayName("")
+        class GetFail {
+        }
+    }
+
+    @Nested
+    @DisplayName("")
+    class Delete {
+        @Nested
+        @DisplayName("")
+        class DeleteSuccess {
+        }
+
+        @Nested
+        @DisplayName("")
+        class DeleteFail {
+        }
+    }
 }
