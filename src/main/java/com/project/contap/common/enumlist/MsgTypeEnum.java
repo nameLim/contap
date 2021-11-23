83c7b4c6fa08e530c837eaf2dc3fca1a377b361a
leeseungjun <lsj2867@gmail.com>
2021-11-24 00:29:48 +0900

    메시지 전송시 분기처리 비트연산으로 변경

--- src/main/java/com/project/contap/common/enumlist/MsgTypeEnum.java
+++ src/main/java/com/project/contap/common/enumlist/MsgTypeEnum.java
@@ -7,12 +7,14 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MsgTypeEnum {
-    CHAT_BOTH(0,"",null),
-    CHAT_EITHER_LOGINON(1,"",null),
-    CHAT_EITHER_LOGOFF(2,"",AlarmEnum.CHAT),
-    SEND_TAP(3,"탭요청을보냈어요",AlarmEnum.TAP_RECEIVE),
-    REJECT_TAP(4,"탭요청을 거절했네요!",AlarmEnum.REJECT_TAP),
-    ACCEPT_TAP(5,"탭요청을 수락했네요!",AlarmEnum.ACCEPT_TAP);
+    CHAT_BOTH(0b000001,"",null),
+    CHAT_EITHER_LOGINON(0b000010,"",null),
+    CHAT_EITHER_LOGOFF(0b000100,"",AlarmEnum.CHAT),
+    SEND_TAP(0b001000,"탭요청을보냈어요",AlarmEnum.TAP_RECEIVE),
+    REJECT_TAP(0b010000,"탭요청을 거절했네요!",AlarmEnum.REJECT_TAP),
+    ACCEPT_TAP(0b100000,"탭요청을 수락했네요!",AlarmEnum.ACCEPT_TAP),
+    TO_ALL(0b000111,"탭요청을 수락했네요!",AlarmEnum.ACCEPT_TAP),
+    TO_USER(0b111010,"탭요청을 수락했네요!",AlarmEnum.ACCEPT_TAP);
 
     private final int value;
     private final String msg;
