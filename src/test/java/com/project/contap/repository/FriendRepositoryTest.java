30a7d1c65289d66f76e79b714b0775cd85694d42
leeseungjun <lsj2867@gmail.com>
2021-11-26 16:11:29 +0900

    Friend Repository 테스트코드 작성
    UserService 테스트코드 수정

--- src/test/java/com/project/contap/repository/FriendRepositoryTest.java
+++ src/test/java/com/project/contap/repository/FriendRepositoryTest.java
@@ -29,13 +29,15 @@ class FriendRepositoryTest {
     private UserRepository userRepository;
 
 
-    Long id1;
-    Long id2;
-    Long id3;
-    Long id4;
+    List<Long> userIds = new ArrayList<>();
+    List<Long> friendIds = new ArrayList<>();
+
 
     @BeforeEach
     void setUp() throws Exception {
+
+        userIds = new ArrayList<>();
+        friendIds = new ArrayList<>();
         User user1 = User.builder()
                 .email("user1@gmail.com")
                 .pw("123")
@@ -57,10 +59,10 @@ class FriendRepositoryTest {
                 .userName("user4")
                 .field(1).build();
 
-        id1 = userRepository.save(user1).getId();
-        id2 = userRepository.save(user2).getId();
-        id3 = userRepository.save(user3).getId();
-        id4 = userRepository.save(user4).getId();
+        userIds.add(userRepository.save(user1).getId());
+        userIds.add(userRepository.save(user2).getId());
+        userIds.add(userRepository.save(user3).getId());
+        userIds.add(userRepository.save(user4).getId());
 
         Friend fir = Friend.builder()
                 .roomId("roomId")
@@ -72,8 +74,8 @@ class FriendRepositoryTest {
                 .me(user2)
                 .you(user1)
                 .build();
-        friendRepository.save(fir);
-        friendRepository.save(sec);
+        friendIds.add(friendRepository.save(fir).getId());
+        friendIds.add(friendRepository.save(sec).getId());
 
         Friend fir1 = Friend.builder()
                 .roomId("roomId")
@@ -85,8 +87,8 @@ class FriendRepositoryTest {
                 .me(user3)
                 .you(user1)
                 .build();
-        friendRepository.save(fir1);
-        friendRepository.save(sec1);
+        friendIds.add(friendRepository.save(fir1).getId());
+        friendIds.add(friendRepository.save(sec1).getId());
 
         Friend fir2 = Friend.builder()
                 .roomId("roomId")
@@ -98,8 +100,8 @@ class FriendRepositoryTest {
                 .me(user4)
                 .you(user1)
                 .build();
-        friendRepository.save(fir2);
-        friendRepository.save(sec2);
+        friendIds.add(friendRepository.save(fir2).getId());
+        friendIds.add(friendRepository.save(sec2).getId());
 
         Friend fir3 = Friend.builder()
                 .roomId("roomId")
@@ -111,18 +113,17 @@ class FriendRepositoryTest {
                 .me(user4)
                 .you(user3)
                 .build();
-        friendRepository.save(fir3);
-        friendRepository.save(sec3);
+        friendIds.add(friendRepository.save(fir3).getId());
+        friendIds.add(friendRepository.save(sec3).getId());
     }
 
     @Test
     @DisplayName("친구관계 체크")
-    void findByCategory01() {
-        User user1 = userRepository.findById(id1).orElse(null);
-        User user2 = userRepository.findById(id2).orElse(null);
-        User user3 = userRepository.findById(id3).orElse(null);
-        User user4 = userRepository.findById(id4).orElse(null);
-
+    void checkFriend() {
+        User user1 = userRepository.findById(userIds.get(0)).orElse(null);
+        User user2 = userRepository.findById(userIds.get(1)).orElse(null);
+        User user3 = userRepository.findById(userIds.get(2)).orElse(null);
+        User user4 = userRepository.findById(userIds.get(3)).orElse(null);
         assertTrue(friendRepository.checkFriend(user1,user2));
         assertTrue(friendRepository.checkFriend(user1,user3));
         assertTrue(friendRepository.checkFriend(user1,user4));
@@ -135,5 +136,42 @@ class FriendRepositoryTest {
         assertFalse(friendRepository.checkFriend(user2,user4));
         assertFalse(friendRepository.checkFriend(user3,user2));
         assertFalse(friendRepository.checkFriend(user4,user2));
+    }
+
+    @Test
+    @DisplayName("Friend 엔티티 불러오기")
+    void getFriend() {
+        User user1 = userRepository.findById(userIds.get(0)).orElse(null);
+        User user2 = userRepository.findById(userIds.get(1)).orElse(null);
+        User user3 = userRepository.findById(userIds.get(2)).orElse(null);
+        User user4 = userRepository.findById(userIds.get(3)).orElse(null);
+        assertNotNull(friendRepository.getFriend(user1,user2));
+        assertNotNull(friendRepository.getFriend(user1,user3));
+        assertNotNull(friendRepository.getFriend(user1,user4));
+        assertNotNull(friendRepository.getFriend(user2,user1));
+        assertNull(friendRepository.getFriend(user2,user3));
+        assertNull(friendRepository.getFriend(user2,user4));
+        assertNotNull(friendRepository.getFriend(user3,user1));
+        assertNull(friendRepository.getFriend(user3,user2));
+        assertNotNull(friendRepository.getFriend(user3,user4));
+        assertNotNull(friendRepository.getFriend(user4,user1));
+        assertNull(friendRepository.getFriend(user4,user2));
+        assertNotNull(friendRepository.getFriend(user4,user3));
+    }
+
+    @Test
+    @DisplayName("User 와 관련된 Friend 엔티티 모두다 불러오기")
+    void getallmyFriend() {
+        for(int i = 0;i < userIds.size();i++)
+        {
+            User user = userRepository.findById(userIds.get(i)).orElse(null);
+            List<Friend> friends = friendRepository.getallmyFriend(user);
+            for(Friend friend : friends)
+            {
+                boolean check1 = friend.getMe().getId() == user.getId();
+                boolean check2 = friend.getYou().getId() == user.getId();
+                assertTrue(check1|check2);
+            }
+        }
     }
 }
